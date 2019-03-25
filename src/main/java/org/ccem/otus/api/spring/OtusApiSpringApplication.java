5916477f97fc52db519c31d036146c2219f0fa96
Fabiano Ramires <fdrtec@gmail.com>
2019-03-24 22:11:38 -0300

    OAS-2 Spring com tomcat e mongodb

---
+++ src/main/java/org/ccem/otus/api/spring/OtusApiSpringApplication.java
@@ -0,0 +1,13 @@
+package org.ccem.otus.api.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtusApiSpringApplication.class, args);
+	}
+
+}
