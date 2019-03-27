0480aa098e82ea266ac47ad7741b320e5ca44008
Fabiano Ramires <fdrtec@gmail.com>
2019-03-27 16:44:34 -0300

    OAS-6 endpoint funcionando no wildfly remoto

--- src/main/java/org/ccem/otus/api/spring/OtusApiSpringApplication.java
+++ src/main/java/org/ccem/otus/api/spring/OtusApiSpringApplication.java
@@ -2,12 +2,19 @@ package org.ccem.otus.api.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OtusApiSpringApplication {
+public class OtusApiSpringApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OtusApiSpringApplication.class, args);
+	}
+
+	@Override
+	protected  SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(getClass());
 	}
 
 }
